68f5ef0ba0bce2407bfcbeae2b23a145d401a57b
fermorher1337 <fermorher1337@gmail.com>
2022-10-10 12:01:36 +0200

    android

---
+++ Acceso a datos/Java/utilidadesF.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+import java.util.*;
+
+public class utilidadesF{
+
+    public static void main(String[] args){
+
+
+
+
+    }
+
+    public void directorioActual (){
+       File dir= new File (".");
+
+        System.out.println("La ruta actual es " +dir.getPath());
+    }
+    
+}
+
+
