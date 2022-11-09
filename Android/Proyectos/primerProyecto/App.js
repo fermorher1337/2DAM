f8794cb7b438a73baa406d8ee73685059f716782
fermorher1337 <fermorher1337@gmail.com>
2022-11-09 14:42:36 +0100

    formulari

--- Android/Proyectos/primerProyecto/App.js
+++ Android/Proyectos/primerProyecto/App.js
@@ -1,117 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import {useState} from 'react';
+import {StyleSheet,Text,View,Button,TextInput,Image,Switch} from  'react-native';
+import { WebView } from 'react-native-webview';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App() {
+    const nombre=""
+    const apellidos=""
+    const edad=""
+    const email=""
+     const [isEnabled, setIsEnabled] = useState(false);
+     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+return(
+<View>
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+<Text>Nombre</Text>
+<TextInput
+style={{
+height:40,
+borderColor:'black',
+borderWidth:2
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+}}
+placeholder="Escribe tu nombre"
+/>
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+<Text>Apellidos</Text>
+<TextInput
+
+style={{
+height:40,
+borderColor:'black',
+borderWidth:2,
+onChangeText={edad=>setEdad(edad)}
+}}
+placeholder="Escribe tus apellidos"
+/>
+
+<Text>Edad</Text>
+<TextInput
+style={{
+height:40,
+borderColor:'black',
+borderWidth:2
+}}
+placeholder="Escribe tu edad"
+/>
+
+<Text>Correo electronico</Text>
+<TextInput
+style={{
+height:40,
+borderColor:'black',
+borderWidth:2
+}}
+placeholder="Escribe tu correo electronico"
+/>
+
+  <Switch
+
+        trackColor={{ false: "#blue", true: "pink" }}
+        thumbColor={isEnabled ? "#red" : "#cyan"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+</View>
+);
 
+
+
+}
 export default App;
+//<Image
+//style={{width:400,height:400}}
+//source={require('./thanos-dance.gif') }
+///>
