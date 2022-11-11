fe8700ae6017b4a3e8453732a92626e8bf49400c
fermorher1337 <fermorher1337@gmail.com>
2022-11-11 14:40:03 +0100

    letsgo

--- Android/Proyectos/primerProyecto/App.js
+++ Android/Proyectos/primerProyecto/App.js
@@ -4,12 +4,18 @@ import { WebView } from 'react-native-webview';
 
 
 function App() {
-    const nombre=""
-    const apellidos=""
-    const edad=""
-    const email=""
-     const [isEnabled, setIsEnabled] = useState(false);
-     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const [nombre, setNombre] = useState()
+    const [apellidos, setApellidos]= useState()
+    const [edad, setEdad]=useState()
+    const [email, setEmail]=useState()
+
+    const [textoBoole, setTextoBoole]=useState(false)
+    const regex= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+
+
 
 return(
 <View>
@@ -19,9 +25,9 @@ return(
 style={{
 height:40,
 borderColor:'black',
-borderWidth:2
-
+borderWidth:1
 }}
+onChangeText={(value)=>setNombre(value)}
 placeholder="Escribe tu nombre"
 />
 
@@ -31,19 +37,22 @@ placeholder="Escribe tu nombre"
 style={{
 height:40,
 borderColor:'black',
-borderWidth:2,
-onChangeText={edad=>setEdad(edad)}
+borderWidth:1,
+
 }}
+onChangeText={(value)=>setApellidos(value)}
 placeholder="Escribe tus apellidos"
 />
 
 <Text>Edad</Text>
 <TextInput
+  keyboardType = 'numeric'
 style={{
 height:40,
 borderColor:'black',
-borderWidth:2
+borderWidth:1
 }}
+onChangeText={(value)=>setEdad(value)}
 placeholder="Escribe tu edad"
 />
 
@@ -52,28 +61,60 @@ placeholder="Escribe tu edad"
 style={{
 height:40,
 borderColor:'black',
-borderWidth:2
+borderWidth:1
 }}
+onChangeText={(value)=>setEmail(value)}
 placeholder="Escribe tu correo electronico"
 />
+<View style={{alignItems: "center"}}>
+        {isEnabled ? <Text style={{ alignItems: 'center',marginTop:5 }}>Hombre</Text> : <Text style={{ alignItems: 'center',marginTop:5 }}>Mujer</Text>}
 
   <Switch
 
-        trackColor={{ false: "#blue", true: "pink" }}
+        trackColor={{ false: "#blue", true: "yellow" }}
         thumbColor={isEnabled ? "#red" : "#cyan"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
+              activeText={'On'}
+              inActiveText={'Off'}
+              backgroundActive={'purple'}
+              backgroundInactive={'gray'}
+        onValueChange={()=> setIsEnabled(previousState => !previousState)}
         value={isEnabled}
       />
+      </View>
+      <Button
+      color='red'
+      title={!textoBoole?"Activar":"Desactivar"}
+      onPress={()=>setTextoBoole(current=>!current)}
+
+      />
+    {
+    textoBoole?
+    <Text>El nombre es {nombre}, los apellidos son {apellidos} y tiene {edad} anhos. Su correo es {email} y su sexo es {isEnabled?"hombre.":"mujer."}</Text>
+    : ""}
+
+
+{textoBoole?(isEnabled?
+<Image
+style={{width:350,height:350}}
+source={require('./thanos-dance.gif') }/>
+:<Image
+ style={{width:350,height:350}}
+ source={require('./shehulk-twerk.gif') }
+/> ):""}
+
+
+
+
+
 
 </View>
+
+
 );
 
 
 
 }
 export default App;
-//<Image
-//style={{width:400,height:400}}
-//source={require('./thanos-dance.gif') }
+
 ///>
