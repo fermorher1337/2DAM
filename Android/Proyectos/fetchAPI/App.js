a0411583c34abdd6bd3c2de3d1ad6b39a5f2f0a5
fermorher1337 <99404483+fermorher1337@users.noreply.github.com>
2023-01-26 23:27:39 +0100

    subida pro hd funsiona

--- Android/Proyectos/fetchAPI/App.js
+++ Android/Proyectos/fetchAPI/App.js
@@ -1,81 +1,111 @@
-import * as React from 'react';
-import { Text, View,SafeAreaView,  FlatList,StyleSheet,StatusBar } from 'react-native';
+import { useEffect, useState } from "react";
+import { Text, View, SafeAreaView, FlatList, StyleSheet, StatusBar, Button, Alert, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import axios from 'axios';
+import React from 'react';
 
 //Fetcheo
+fetch("http://172.27.112.1:8080/fruits")
+  .then((response) => response.json())
+  .then((fruits) => console.log(fruits));
+//
 
-const baseUrl = 'http://172.23.64.1:8080';
-axios.get(`${baseUrl}/fruits`).then((response) => {
-  console.log(response.data);
-});
 
-// Gets
-const fetchNombre = async () => {
-  const url = `${baseUrl}/fruits/1`;
-  const response = await axios.get(url);
-  console.log(response.data);
+function posteoFruta(nombre,precio){
+  fetch('http://172.27.112.1:8080/fruits', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: nombre,
+      price: precio
+    })
+  })
+    .then(console.log("Datos enviados: "+ nombre+"-"+precio));
 }
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
-const Item = ({title}) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
+    backgroundColor: '#78dcde',
+    padding: 10,
     marginVertical: 8,
     marginHorizontal: 16,
   },
   title: {
     fontSize: 32,
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
 
+
 function HomeScreen() {
-  
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch('http://172.27.112.1:8080/fruits')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Listado de frutas</Text>
-      <SafeAreaView style={styles.container}>
       <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item title={item.title} />}
-        keyExtractor={item => item.id}
+        data={data}
+        keyExtractor={({ id }, index) => id}
+        renderItem={({ item }) => (
+          <Text>{item.name + '.' + item.price}</Text>
+        )}
       />
-    </SafeAreaView>
     </View>
   );
 }
 
 function PostScreen() {
+  const [nombre, onChangeNombre] = React.useState('');
+  const [precio, onChangePrecio] = React.useState('');
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Introduce la fruta a anadir.</Text>
-      <Text>Nombre.</Text>
-      <Text>Precio.</Text>
 
+  
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNombre}
+        value={nombre}
+        placeholder="Introduce el nombre"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePrecio}
+        value={precio}
+        placeholder="Introduce el precio"
+        keyboardType="numeric"
+      />
+
+      <Button
+       onPress={() => posteoFruta(nombre,precio)}
+        title="Posteo a la API"
+        color="#841584"
+
+      />
 
     </View>
   );
@@ -88,7 +118,7 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={PostScreen} />
+        <Tab.Screen name="Post de fruta" component={PostScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
